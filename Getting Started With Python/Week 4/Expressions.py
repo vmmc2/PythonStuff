168b4f908449db1f068abc5e46a9e3608534b828
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-06-08 11:36:53 -0700

    Create Expressions.py

---
+++ Getting Started With Python/Week 4/Expressions.py
@@ -0,0 +1,7 @@
+# Constants
+
+# Constants are fixed values such as numbers, letters and strings are called 'constants' because their value does not change.
+# Numeric constants are as you expect.
+# String constants can use single quotes ('') or double quotes ("").
+
+greeting = "Hello World"
