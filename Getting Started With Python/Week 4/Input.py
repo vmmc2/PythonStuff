7f5f44c76149f5ca645505829a63f9a9de3dab29
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-06-08 12:58:13 -0700

    Create Input.py

---
+++ Getting Started With Python/Week 4/Input.py
@@ -0,0 +1,8 @@
+# Input in Python
+
+# We can instruct Python to pause and read data from the user using the input() function.
+# The input() function returns a string (str).
+# Take a look at the example below:
+
+name = input("Who are you?")
+print("Hi, my name is: ", name)
