1df5e102c704ad1c1ac6e0eea3e6a6ca44d10fab
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-06-08 13:06:47 -0700

    Update Input.py

--- Getting Started With Python/Week 4/Input.py
+++ Getting Started With Python/Week 4/Input.py
@@ -6,3 +6,10 @@
 
 name = input("Who are you?")
 print("Hi, my name is: ", name)
+
+# Simple Exercise:
+# European Floor: 0
+# USA Floor: 1
+eufloor = input("In which floor you are at?")
+usfloor = int(eufloor) + 1
+print("US Floor is: ", usfloor)
