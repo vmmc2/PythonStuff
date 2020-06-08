f5cf3ab696ba8b79ede056ca04b7b4c74e3ab0c8
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-06-08 11:40:17 -0700

    Update Expressions.py

--- Getting Started With Python/Week 4/Expressions.py
+++ Getting Started With Python/Week 4/Expressions.py
@@ -1,8 +1,11 @@
 # Constants
-
 # Constants are fixed values such as numbers, letters and strings are called 'constants' because their value does not change.
 # Numeric constants are as you expect.
 # String constants can use single quotes ('') or double quotes (""). The same applies to letter constants.
 
 greeting = "Hello World"
 print(greeting) #it is going to print the string "Hello World"
+
+
+# Reserved Words
+# You cannot use Reserved Words as variables names, identifiers, functions, classes.
