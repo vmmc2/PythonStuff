ec3f443afe6f7febc0acf1fa6fc920020f24f26b
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-06-09 11:06:49 -0700

    Create Functions.py

---
+++ Getting Started With Python/Week 6/Functions.py
@@ -0,0 +1,18 @@
+# Functions
+
+# We create a new function in Python by using the "def" keyword followed by optional parameters in parentheses.
+# We indent the body of the function (using 4 spaces).
+# This defines the function but does not execute the body of the function.
+
+def print_lyrics():
+    print("I am a Lumberjack, and I am okay.")
+    print("I sleep all night and I work all day.")
+    return
+
+def main():
+    print("Hello World!")
+    print_lyrics()
+
+
+if __init__ == "__main__":
+    main()
