3aadf10bbad142c6865e821d4ca0a1f73a02f16d
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-06-09 10:46:46 -0700

    Create Type Conversions.py

---
+++ Getting Started With Python/Week 6/Type Conversions.py
@@ -0,0 +1,8 @@
+# Type Conversions
+
+# When you put an integer and a floating-point in an expression, the integer is implicitly converted to a float.
+# You can control this using the built-in functions: int() and float()
+
+# Look at the example below:
+a = float(99) / 100 
+print(a) # it is going to print: 0.99
