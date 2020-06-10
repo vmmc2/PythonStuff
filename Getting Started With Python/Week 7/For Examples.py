b41ef702657a04430a69b0571a7518564270eb43
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-06-10 08:07:44 -0700

    Create For Examples.py

---
+++ Getting Started With Python/Week 7/For Examples.py
@@ -0,0 +1,14 @@
+def main():
+    # example of for loops
+    friends = ['Gleen', 'Rick', 'Carl', 'Daryl']
+    for friend in friends:
+      print("Happy Birthday,", friend)
+    
+    numbers = [5, 4, 3, 2, 1]
+    for number in numbers:
+      print(number)
+    print("Blastoff!!")
+
+
+
+main()
