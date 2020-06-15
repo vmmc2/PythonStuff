e25463ca494b5980d197920445936a8c60cfd8f0
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-06-15 07:10:27 -0700

    Create String Library.py

---
+++ Python Data Structures/Week 1/String Library.py
@@ -0,0 +1,12 @@
+# String Library.
+
+# Python has a number of string functions which are in the string library.
+# These functions are already built into every single string - we invoke them by appending the function to the string variable.
+# These functions do not modify the original string. Instead, they return a new string that has been altered.
+
+greet = "Hello Bob"
+print(greet)
+zap = greet.lower() # pego a string greet, deixo todos os seus caracterers como letras minusculas e retorno essa nova string.
+print(zap)
+
+print("Hello There, Baby".lower())
