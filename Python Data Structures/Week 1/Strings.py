3b98d1c1c8266ee7c3093ebdd7ce9b4060091de1
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-06-15 06:11:54 -0700

    Create Strings.py

---
+++ Python Data Structures/Week 1/Strings.py
@@ -0,0 +1,21 @@
+# String Data Type
+
+# A string is a sequence of characters.
+# A string literal uses single-quotes or double-quotes: 'Hello World' or "Hello World".
+# For strings, '+' means concatenate.
+# When a string contains numbers, it is still a string.
+# We can convert a string containing numbers into a number by using the function int()
+
+def main():
+    fruit = 'banana'
+    letter = fruit[1]
+    print(letter) # it is going to print 'a'
+    
+    x = 3
+    w = fruit[x - 1]
+    print(w) # it is going to print 'n'
+
+
+
+if __init__ == "__main__":
+    main()
