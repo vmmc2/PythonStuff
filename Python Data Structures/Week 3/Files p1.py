b8e6516053b6c1e6b5d292d2e614b9e227bb4839
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-06-16 14:02:00 -0700

    Update Files p1.py

--- Python Data Structures/Week 3/Files p1.py
+++ Python Data Structures/Week 3/Files p1.py
@@ -1,13 +1,21 @@
-# Opening a File
+# OPENING A FILE
 
 # Before we can read the contents of a file, we must tell Python which file we are going to work with and also what we will be doing with the file.
 # This is done with the open() function.
 # This open() function returns a "file handle" - a variable used to perform operations on the file.
 
-# Example
+# Example:
 
 # handle = open(filename, mode)
 # -> handle is a variable used to manipulate the file.
 # -> filename is the name of the file that we are going to interact with. It is a string.
 # -> mode is optional and should be 'r' if we are planning to read the file. Otherwise, it should be 'w' if we are planning to write to the file.
 fhand = open('mbox.txt' , 'r')
+
+# When files are missing and we try to execute the open() function on it, we will get a traceback. A way to avoid this is using the try-except block.
+
+# THE NEWLINE CHARACTER
+
+# We use a special character called "newline" to indicate where a line ends.
+# We represent it inside a string by typing: '\n'.
+# Newline is still one character - not two. It is considered as a character that is inside the string.
