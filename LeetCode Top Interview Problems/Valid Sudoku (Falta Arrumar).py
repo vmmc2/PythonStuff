7d8a90eafa181ae712b2ecedd86c088b8f5fe01a
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-06-17 12:13:47 -0700

    Create Valid Sudoku (Falta Arrumar).py

---
+++ LeetCode Top Interview Problems/Valid Sudoku (Falta Arrumar).py
@@ -0,0 +1,53 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        valid = True
+        # First Step: Check each line.
+        freq = {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0}
+        for i in range(0, 9):
+            # traversing through all cells of a row.
+            freq = {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0}
+            for j in range(0, 9):
+                if board[i][j] == '.': continue
+                else:
+                    freq[board[i][j]] += 1
+                    if freq[board[i][j]] > 1:
+                        valid = False
+        if valid == False:
+            return valid
+        
+        # Second step: Check each column.
+        for j in range(0, 9):
+            # traversing through all cells of a column.
+            freq = {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0}
+            for i in range(0, 9):
+                if board[i][j] == '.': continue
+                else:
+                    freq[board[i][j]] += 1
+                    if freq[board[i][j]] > 1:
+                        valid = False
+        if valid == False:
+            return valid
+        
+        # Third Step: Check each sub-square
+        i = 0
+        j = 0
+        a = 0
+        b = 0
+        while i <=6:
+            freq = {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0}
+            while j <= 6:
+                a = 0
+                b = 0
+                while a < 3:
+                    while b < 3:
+                        if board[i + a][j + b] == '.': continue
+                        else:
+                            freq[board[i + a][j + b]] += 1
+                            if freq[board[i + a][j + b]] > 1:
+                                valid = False
+                        b += 1
+                    a += 1
+                j += 3
+            i += 3
+        return valid
+                
