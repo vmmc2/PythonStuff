64d31ccf0f88e346448225cbb93fdb346c92fb88
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-06-29 08:28:38 -0700

    Create Search in Rotated Sorted Array.py

---
+++ LeetCode Top Interview Problems/Search in Rotated Sorted Array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        answer = -1
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right)//2
+            if nums[mid] == target:
+                answer = mid
+                return answer
+            # Tem que achar qual das duas metades do array ta ordenada...
+            if nums[left] <= nums[mid]: # primeira metade ta ordenada
+                if nums[left] < target and target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            elif nums[mid] <= nums[right]: # segunda metade ta ordenada 
+                if nums[mid] < target and target < nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return answer
