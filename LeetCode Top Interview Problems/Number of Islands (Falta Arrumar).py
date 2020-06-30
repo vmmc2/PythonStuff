b8d37a3aac0b531d7c9dccf075b6afaf47f146ad
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-06-30 09:02:46 -0700

    Update Number of Islands (Falta Arrumar).py

--- LeetCode Top Interview Problems/Number of Islands (Falta Arrumar).py
+++ LeetCode Top Interview Problems/Number of Islands (Falta Arrumar).py
@@ -1,42 +1,37 @@
 visited = []
 ggrid = []
-answer = 0
-dx = [1,-1, 0, 0]
-dy = [0, 0, 1,-1]
 
-def preprocess(m: int, n: int) -> None:
+def floodfill(x: int, y: int, m: int, n: int) -> None:
     global visited
-    visited = [[0 for i in range(0, n)] for j in range(0, m)]
-    return
-
-def floodfill(i: int, j: int, m: int, n: int) -> None:
-    visited[i][j] = 1 # visitando a celula
-    for k in range(0, 4):
-        global dx
-        global dy
-        newi = i + dx[k]
-        newj = j + dx[k]
-        if(newi >= 0 and newi < m and newj >= 0 and newj < n):
-            if(visited[newi][newj] == 0 and ggrid[newi][newj] == '1'):
-                floodfill(newi, newj, m, n)
+    visited[x][y] = 1
+    dx = [1,-1, 0, 0]
+    dy = [0, 0, 1,-1]
+    for i in range(0, 4):
+        newx = x + dx[i]
+        newy = y + dy[i]
+        global ggrid
+        if newx >= 0 and newx < m and newy >= 0 and newy < n and ggrid[newx][newy] == "1" and visited[newx][newy] == 0:
+            floodfill(newx, newy, m, n)
     return
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # Treating corner cases first...
+        # Treating the corner cases first:
         if len(grid) == 0:
             return 0
         if len(grid[0]) == 0:
             return 0
         # Treating the general cases now...
-        preprocess(len(grid), len(grid[0]))
+        answer = 0
         global ggrid
         ggrid = grid
-        global answer
-        answer = 0
+        global visited
+        visited = [[0 for i in range(0, len(grid[0]))] for j in range(0, len(grid))]
+        print(ggrid)
+        print(visited)
         for i in range(0, len(grid)):
             for j in range(0, len(grid[0])):
-                if visited[i][j] == 0:
+                if ggrid[i][j] == "1" and visited[i][j] == 0:
+                    answer += 1
                     floodfill(i, j, len(grid), len(grid[0]))
-                    print(visited)
         return answer
