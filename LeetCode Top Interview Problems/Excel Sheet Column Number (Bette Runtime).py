19778edfd99d9d4286d0e93dd66b007d26d75eb0
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-07-16 06:23:41 -0700

    Create Excel Sheet Column Number (Bette Runtime).py

---
+++ LeetCode Top Interview Problems/Excel Sheet Column Number (Bette Runtime).py
@@ -0,0 +1,15 @@
+import math
+
+class Solution:
+    def titleToNumber(self, s: str) -> int:
+        # Solucao para a questao:
+        # Complexidade Temporal: O(n)
+        # Complexidade Espacial: O(1)
+        exp = 0
+        answer = 0
+        curr = len(s) - 1
+        while curr >= 0:
+            answer += math.pow(26, exp) * (ord(s[curr]) - ord('A') + 1)
+            curr -= 1
+            exp += 1
+        return int(answer)
