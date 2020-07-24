3db32e2f169c10c424958f723540f9781ff52751
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-07-24 15:25:07 -0700

    Update Regex Example 2.py

--- Using Python to Access Web Data/Week 2/Regex Example 2.py
+++ Using Python to Access Web Data/Week 2/Regex Example 2.py
@@ -4,7 +4,7 @@ def searchWithRegex(fileName):
   f = open(fileName)
   for line in f:
     line = line.strip()
-    if re.search("^From: ", line) == True:
+    if re.search("^From: ", line) == True: # Indica que o casamento de strings/padroes deve acontecer logo no inicio da linha. Em outras palavras, o primeiro caractere da linha deve ser 'F'.
       print(line)
   return
   
