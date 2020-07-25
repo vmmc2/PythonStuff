a868552f773524875602381dfbdac9942b5d7f67
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-07-25 14:15:52 -0700

    Update sockets.py

--- Using Python to Access Web Data/Week 3/sockets.py
+++ Using Python to Access Web Data/Week 3/sockets.py
@@ -3,4 +3,4 @@
 import socket  # necessario dar import nessa lib para trabalharmos com os sockets.
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Nessa linha, a gente faz a criacao do nosso socket (que vai servir para estabelecermos a comunicacao entre a nossa maquina e um servidor, por exemplo)
-mysock.connet(("data.pr4e.org", 80)) # Nessa linha, a gente ta fazendo de fato a conexao entre a nossa maquina e um servidor. Para isso, a gente deve fornecer o host e uma porta.
+mysock.connect(("data.pr4e.org", 80)) # Nessa linha, a gente ta fazendo de fato a conexao entre a nossa maquina e um servidor. Para isso, a gente deve fornecer o host e uma porta.
