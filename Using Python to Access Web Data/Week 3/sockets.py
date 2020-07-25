a6ccf6a125925b8160f6f56b95ba4a7b103879d3
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-07-25 15:08:29 -0700

    Update sockets.py

--- Using Python to Access Web Data/Week 3/sockets.py
+++ Using Python to Access Web Data/Week 3/sockets.py
@@ -4,3 +4,13 @@ import socket  # necessario dar import nessa lib para trabalharmos com os socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Nessa linha, a gente faz a criacao do nosso socket (que vai servir para estabelecermos a comunicacao entre a nossa maquina e um servidor, por exemplo)
 mysock.connect(("data.pr4e.org", 80)) # Nessa linha, a gente ta fazendo de fato a conexao entre a nossa maquina e um servidor. Para isso, a gente deve fornecer o host e uma porta.
+
+cmd = "GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n".encode() # Isso aqui eh basicamente o comando que vamos enviar para o servidor. No caso, estamos enviando um comando do tipo GET para solicitar dados/documento.
+mysock.send(cmd) # Enviando o nosso comando para o servidor.
+
+while True:
+  data = mysock.recv(512) # Comando para recebr os dados provenientes do servidor ao qual nos conectamos. Nesse caso, podemos receber apenas 512 caracteres por vez.
+  if len(data) < 1:
+    break # Isso quer dizer que nao estamos mais recebendo dados do servidor.
+  print(data.decode())
+mysock.close() # Acabamos de pegar os dados que queriamos, fechamos o socket, interrompendo a conexao com o servidor.
