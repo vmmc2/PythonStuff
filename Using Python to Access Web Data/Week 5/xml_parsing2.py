0f83320ae89abfceb6d2f7aacc8db61493b2e205
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-07-28 08:30:25 -0700

    Update xml_parsing2.py

--- Using Python to Access Web Data/Week 5/xml_parsing2.py
+++ Using Python to Access Web Data/Week 5/xml_parsing2.py
@@ -1,1 +1,22 @@
 import xml.etree.ElementTree as ET
+
+s = '''<stuff>
+  <users>
+    <user x="2">
+      <id>001</id>
+      <name>Chuck</name>
+    </user>
+    <user x="7">
+      <id>009</id>
+      <name>Brent</name>
+    </user>
+  </users>
+</stuff>'''
+
+stuff = ET.fromstring(s)
+lst = stuff.findall('users/user')
+print("User count:", len(lst))
+for item in lst:
+  print('Name:', item.find('name').text)
+  print('Id:', item.find('id').text)
+  print('Attribute:', item.get('x'))
