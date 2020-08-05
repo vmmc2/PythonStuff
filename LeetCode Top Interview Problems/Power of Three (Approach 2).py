682d598c9fa8e712877fdc01db94fe7fe592933f
Victor Miguel de Morais Costa <39977631+vmmc2@users.noreply.github.com>
2020-08-05 12:45:00 -0700

    Update Power of Three (Approach 2).py

--- LeetCode Top Interview Problems/Power of Three (Approach 2).py
+++ LeetCode Top Interview Problems/Power of Three (Approach 2).py
@@ -10,3 +10,24 @@ class Solution:
             return True
         else:
             return False
+
+###################################################################################################################################################################################        
+import math
+
+class Solution2:
+    def isPowerOfThree(self, n: int) -> bool:
+        # Corner case.
+        if n <= 0:
+            return False
+        # O problema garante para a gente que todos o numeros fornecidos como input sao de 32 bits.
+        # Isso significa que: o maior numero que a gente pode receber eh (2**31) - 1
+        # Precisamos descobrir qual eh o maior numero nesse range [0, (2**31) - 1] que eh uma potencia de 3.
+        # Para isso, basta fazer logo na base 3.
+        exp = math.floor(math.log(2**31 - 1, 3))
+        biggestpowerof3 = 3**exp
+        # Feito isso, para descobrirmos se um numero X eh uma potencia de 3, basta ver se o resto da divisao de:
+        # biggestpowerof3//X == 0
+        if biggestpowerof3 % n == 0:
+            return True
+        else:
+            return False
